af1371c0bb240f02070f1cc24f4ace5fb13d8658
james_jjyang <ieee820@gmail.com>
2019-01-03 15:02:07 +0800

    Create make_dict_for_nlp.py

---
+++ make_dict_for_nlp.py
@@ -0,0 +1,18 @@
+import zipfile
+
+with zipfile.ZipFile('./data/jaychou_lyrics.txt.zip') as zin:
+    with zin.open('jaychou_lyrics.txt') as f:
+        corpus_chars = f.read().decode('utf-8')
+print(corpus_chars[:40])
+corpus_chars = corpus_chars.replace('\n', ' ').replace('\r', ' ')
+corpus_chars = corpus_chars[0:10000]
+
+idx_to_char = list(set(corpus_chars))
+char_to_idx = dict([(char, i) for i, char in enumerate(idx_to_char)])
+vocab_size = len(char_to_idx)
+print(vocab_size)
+
+corpus_indices = [char_to_idx[char] for char in corpus_chars]
+sample = corpus_indices[:20]
+print('chars:', ''.join([idx_to_char[idx] for idx in sample]))
+print('indices:', sample)
