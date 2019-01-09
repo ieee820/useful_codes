8739e76583352d68c7a8d96a129cd18e376884e7
james_jjyang <ieee820@gmail.com>
2019-01-09 18:28:22 +0800

    Update pytorch_tips.py

--- pytorch_tips.py
+++ pytorch_tips.py
@@ -4,3 +4,7 @@ input = torch.randn(128, 20)
 output = m(input)
 print(output.size())
 torch.Size([128, 30])
+
+#格式化多张图片到一张图片：(20,w,h) -> (20,1,w,h)
+images = [transform(image) for image in images]
+images = torch.cat([t.unsqueeze(0) for t in images], 0)
