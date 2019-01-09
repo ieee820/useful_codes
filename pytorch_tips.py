c37ead7aa1ffdec2eedb3ea8c343b8d55c816dcb
james_jjyang <ieee820@gmail.com>
2019-01-09 18:53:54 +0800

    Update pytorch_tips.py

--- pytorch_tips.py
+++ pytorch_tips.py
@@ -8,3 +8,10 @@ torch.Size([128, 30])
 #格式化多张图片到一张图片：(20,w,h) -> (20,1,w,h)
 images = [transform(image) for image in images]
 images = torch.cat([t.unsqueeze(0) for t in images], 0)
+
+#在训练OCR模型时，快速获得字典和字符的index
+self.alphabet = alphabet + 'ç'  # for `-1` index
+self.dict = {}
+for i, char in enumerate(alphabet):
+    # NOTE: 0 is reserved for 'blank' required by wrap_ctc
+    self.dict[char] = i + 1
