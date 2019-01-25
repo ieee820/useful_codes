0789133e02201b82abee02cf08b30eb62f108392
james_jjyang <ieee820@gmail.com>
2019-01-25 09:51:02 +0800

    Update useful_bash.sh

--- useful_bash.sh
+++ useful_bash.sh
@@ -1,2 +1,8 @@
 #随机乱序文件内容
 shuf file.txt | more    
+# 文件内容去重复：
+ls ./*.jpg | sort -u | uniq | wc -l
+#查找符合条件的文件并copy
+cat ../pre_100.txt | xargs mv -t /d/pre_100_test/
+cat ../pre_100.txt | xargs rm -rf 
+cat ../pre_100.txt | xargs cp -t /d/pre_100_test/
