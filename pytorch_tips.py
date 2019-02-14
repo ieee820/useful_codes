c9904255c63745d201bacf9e4d445ebef9390f58
james_jjyang <ieee820@gmail.com>
2019-02-14 19:18:58 +0800

    Update pytorch_tips.py

--- pytorch_tips.py
+++ pytorch_tips.py
@@ -21,3 +21,30 @@ out_HR = net(net_input)
 dot = make_dot(out_HR, params=dict(net.named_parameters()))
 dot.render(filename='./net')
 
+#获得网络的summary，包括每一层输出
+from torchsummary import summary
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # PyTorch v0.4.0
+model = Net().to(device)
+#有些情况不用输入第一个维度
+summary(model, (1, 28, 28))
+#
+
