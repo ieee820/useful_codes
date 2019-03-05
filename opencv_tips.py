5db31d71b23b4464c31937463e972183b60ee3a5
james_jjyang <ieee820@gmail.com>
2019-03-05 14:06:18 +0800

    Update opencv_tips.py

--- opencv_tips.py
+++ opencv_tips.py
@@ -34,3 +34,34 @@
                 crop_img, [cv2.IMWRITE_PNG_COMPRESSION, compression_level])
     
     return 'Processing {:s} ...'.format(img_name)
+  
+  # 将BGR转为RGB
+ # BGR to RGB, HWC to CHW, numpy to tensor
+        if img_HR.shape[2] == 3:
+            img_HR = img_HR[:, :, [2, 1, 0]]
+            img_LR = img_LR[:, :, [2, 1, 0]] 
+  
+  
+  #rgb2ycbcr
+  def rgb2ycbcr(img, only_y=True):
+    '''same as matlab rgb2ycbcr
+    only_y: only return Y channel
+    Input:
+        uint8, [0, 255]
+        float, [0, 1]
+    '''
+    in_img_type = img.dtype
+    img.astype(np.float32)
+    if in_img_type != np.uint8:
+        img *= 255.
+    # convert
+    if only_y:
+        rlt = np.dot(img, [65.481, 128.553, 24.966]) / 255.0 + 16.0
+    else:
+        rlt = np.matmul(img, [[65.481, -37.797, 112.0], [128.553, -74.203, -93.786],
+                              [24.966, 112.0, -18.214]]) / 255.0 + [16, 128, 128]
+    if in_img_type == np.uint8:
+        rlt = rlt.round()
+    else:
+        rlt /= 255.
+    return rlt.astype(in_img_type)
